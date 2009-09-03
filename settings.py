4fc269b8c1896ea4529f3952af76cd529247bc9a
Richard West <rwest@mit.edu>
2009-09-03 11:16:09 -0400

    Moved templates/kinetics into kinetics/templates and the base template up to templates/base.html. Created settings_production.py for production-server. Added .gitignore to ignore .pyc files, and removed a couple that were committed earlier by mistake

--- settings.py
+++ settings.py
@@ -55,7 +55,7 @@ SECRET_KEY = '5sa%b%ywx7&j1*a(*nv*aqrhf258x*()9#s90#xi*00kn^y*yo'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.app_directories.load_template_source', # loads from app_name/templates/
     'django.template.loaders.filesystem.load_template_source',
 
 #     'django.template.loaders.eggs.load_template_source',
